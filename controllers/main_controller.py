e224fffb143960d4a9414f2819c595b2d727eb6f
PavelGR <grigorevpavelevgenevich@gmail.com>
2024-07-30 20:38:18 +0300

    change main menu view

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -17,10 +17,8 @@ class MainController():
     def search_ocr_results(self):
         search_term = self.view.lineEdit.text()
         results = self.db_controller.search_exact_match(search_term)
-        for result in results:
-            print(
-                # f"Путь к изображению: {result[0]}, OCR результат: {result[1]}, Время: {result[2]}")
-                f"Путь к изображению: {result[0]}")
+        image_paths = [result[0] for result in results]
+        self.view.display_images(image_paths)
 
     def run(self):
         self.view.show()
--- views/main_view.py
+++ views/main_view.py
@@ -1,5 +1,8 @@
 import logging
-from PyQt6 import QtWidgets, uic, QtGui
+from PyQt6 import QtWidgets, uic, QtGui, QtCore
+from PyQt6.QtWidgets import QGraphicsScene, QGraphicsPixmapItem
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtCore import QTimer
 from logger import setup_logging
 
 setup_logging()
@@ -9,7 +12,19 @@ class MainView(QtWidgets.QMainWindow):
     def __init__(self):
         super(MainView, self).__init__()
         uic.loadUi('views/ui/mainwindow.ui', self)
+        self.image_paths = []
 
     def closeEvent(self, event: QtGui.QCloseEvent) -> None:
         logging.info("Application is closing")
         event.accept()
+
+    # def resizeEvent(self, event: QtGui.QResizeEvent) -> None:
+    #     super().resizeEvent(event)
+    #     self.resize_timer.start(200)  # Delay resize handling by 200 ms
+
+    # def update_display_images(self):
+    #     if self.image_paths:
+    #         self.display_images(self.image_paths)
+
+    def display_images(self, image_paths):
+        pass
